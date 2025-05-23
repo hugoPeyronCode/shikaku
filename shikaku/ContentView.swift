e1033725b9b94ead5d2373f8405f72ec63557b90
HugoPVoodoo <hugo.peyron@voodoo.io>
2025-05-23 17:12:29 +0200

    Initial Commit

---
+++ shikaku/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  shikaku
+//
+//  Created by Hugo Peyron on 23/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ shikaku/shikakuApp.swift
@@ -0,0 +1,17 @@
+//
+//  shikakuApp.swift
+//  shikaku
+//
+//  Created by Hugo Peyron on 23/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct shikakuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
